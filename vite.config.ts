da399812eac3aa921db52e066147da5747704366
marsokar <aboomarsokar@gmail.com>
2025-05-24 11:58:31 +0300

    try to deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/app",
+  // base: "/app",
   plugins: [vue(), tailwindcss()],
 });
